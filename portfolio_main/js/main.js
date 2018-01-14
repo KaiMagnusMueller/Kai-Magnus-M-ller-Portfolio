53cb0d852529222101d05d3b56490bc102ad811a
Kai Magnus Müller <kai.mueller@hfg-gmuend.de>
2018-01-14 21:05:35 +0100

    - Added Sofia Pro type for headings
    - Added Application Design project
    - Added Invention Design project
    
    - Optimizations to existing texts
    - Minor css adjustments

--- portfolio_main/js/main.js
+++ portfolio_main/js/main.js
@@ -1,14 +1,16 @@
 var tabNum = 0;
-
+var videos
 $(document).ready(function () {
-    var videos = document.getElementsByClassName("interactive")
+    videos = document.getElementsByClassName("interactive")
     var captions = document.getElementsByClassName("interacCaption")
     console.log(videos)
 
     for (var i = 0; i < videos.length; i++) {
+        videos[i].pause()
         videos[i].hidden = true
         captions[i].hidden = true
     }
+    videos[0].play()
     videos[0].hidden = false
     captions[0].hidden = false
 
@@ -17,11 +19,7 @@ $(document).ready(function () {
         $(this).toggleClass("active")
 
         tabNum = $(this).attr("id")
-        for (var i = 0; i < videos.length; i++) {
-            videos[i].hidden = true
-            captions[i].hidden = true
-            videos[i].pause()
-        }
+        hide()
         videos[tabNum].hidden = false
         videos[tabNum].currentTime = 0
         videos[tabNum].play()
@@ -32,17 +30,37 @@ $(document).ready(function () {
         tabNum++
         $("#" + mod(tabNum)).toggleClass("active")
         $("#" + (mod(tabNum-1))).toggleClass("active")
-        for (var i = 0; i < videos.length; i++) {
-            videos[i].hidden = true
-            captions[i].hidden = true
-        }
+        hide()
         videos[mod(tabNum)].hidden = false
         videos[mod(tabNum)].play()
         captions[mod(tabNum)].hidden = false
     });
 
+    $("video").click(function() {
+        if (this.paused === false) {
+            this.pause();
+        } else {
+            this.play();
+        }
+    });
+    //
+    // $("video").hover(function () {
+    //     if(this.paused === false) {
+    //         this.child().css("background-image", "url(../img/pause.svg)")
+    //     } else {
+    //         this.child().css("background-image", "url(../img/play.svg)")
+    //     }
+    // })
+
     function mod(_tabNum) {
         // console.log(_tabNum % 5)
-        return _tabNum % 5
+        return _tabNum % videos.length
+    }
+    function hide() {
+        for (var i = 0; i < videos.length; i++) {
+            videos[i].hidden = true
+            captions[i].hidden = true
+            videos[i].pause()
+        }
     }
 })
