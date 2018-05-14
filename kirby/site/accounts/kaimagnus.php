80152424d7b23017a6607aeca4c14cc5f0ad082b
KaiMagnusMueller <ndsignin@outlook.de>
2018-05-14 19:25:17 +0200

    initial kirby placeholder

---
+++ kirby/site/accounts/kaimagnus.php
@@ -0,0 +1,8 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+username: kaimagnus
+password: >
+  $2a$10$fE3fpoeg8hd8zWRU9jCsSeDYeJoXNySBnLl5pybWidICXPQow8xwK
+email: kai.mueller@hfg-gmuend.de
+language: en
+role: admin
